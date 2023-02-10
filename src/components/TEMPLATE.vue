dcec2246977d07a2f3d803c1e297510d59ab1619
GerardoITA <gerardo.giusti@tiscali.it>
2023-02-10 23:21:59 +0100

    Aggiunte dipendenze scss, axios e creato template component

---
+++ src/components/TEMPLATE.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+Hello World
+</template>
+
+<style lang="scss" scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
