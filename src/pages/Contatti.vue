c8742900027410f835822cf2379d9e83437cce67
GerardoITA <gerardo.giusti@tiscali.it>
2023-02-12 00:46:19 +0100

    aggiornamento sezione Contatti

--- src/pages/Contatti.vue
+++ src/pages/Contatti.vue
@@ -21,13 +21,41 @@ export default {
 
     </Header>
     <main>
-        <h2>Contatti</h2>
-        <p>Qui puoi trovare i miei contatti</p>
+        <h2>Qui puoi trovare i miei contatti</h2>
+        <hr>
+        <section>
+            <h3>Github</h3>
+            <a href="https://github.com/GerardoITA" target="_blank">https://github.com/GerardoITA</a>
+            <p>A questo link puoi trovare il mio account github, con all'interno tutti i miei progetti, sia personali che del corso Boolean, dai primi 'Hello World' su html fino ai più recenti, tra cui questo sito.</p>
+        </section>
+        <hr class="smallerHr">
+        <section>
+            <h3>Email</h3>
+            <i>email@myemail.it</i>
+            <p>Per contattarmi puoi scrivere un'email a questo indirizzo.</p>
+        </section>
+        <hr class="smallerHr">
+        <section>
+            <h3>Cellulare</h3>
+            <i>13456789</i>
+            <p>Puoi scrivermi su Whatsapp a questo numero - risponderò il prima possibile.</p>
+        </section>
+        <hr class="smallerHr">
     </main>
 </div>
 </template>
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
-
-
+main {
+    hr {
+        color: $MineShaft;
+        margin: 3rem 0 3rem;
+    }
+    * {
+        margin: 0.5rem 0;
+    }
+    a {
+        color: $ButterflyBush;
+    }
+}
 </style>
